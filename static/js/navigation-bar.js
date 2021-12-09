f87a8a658a9c96a4661ed9df4cf2636cbcfcc781
Harshu Prasad Shukla <84216006+Harshu9892@users.noreply.github.com>
2021-12-09 13:14:19 +0530

    Completely Designed Category Section of the Forum

---
+++ static/js/navigation-bar.js
@@ -0,0 +1,6 @@
+// const companyLogoNavigationBar = document.getElementById('companyLogoNavigationBar');
+// companyLogoNavigationBar.addEventListener('click',()=>{
+//     window.location.href = ''
+// })
+
+// let loginSignupButton = document.getElementsByClassName('loginSignupButton');
