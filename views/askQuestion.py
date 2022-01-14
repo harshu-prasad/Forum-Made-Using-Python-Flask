a95b2d9049363e71efcc726f9422ddfbe1e7ba34
Harshu Prasad Shukla <84216006+Harshu9892@users.noreply.github.com>
2022-01-14 06:42:19 +0530

    Added add a question feature in the website

--- views/askQuestion.py
+++ views/askQuestion.py
@@ -1,8 +1,34 @@
-from flask import Blueprint,render_template,request
+from flask import Blueprint,request,redirect,flash
+from . import db # Importing Database Variable
+from .models import User # Importing User from models.py to access Name of User
+from .models import Question
+import uuid
 # from flask.typing import StatusCode
 
 askQuestion = Blueprint('askQuestion',__name__,template_folder='templates') # Creating Blueprint to adress /askQuestion endpoint
 
-@askQuestion.route('/<string:userName>',methods=['POST'])
-def askQuestionFunction():
-    return render_template('home.html')
+@askQuestion.route('/<string:profileUserName>',methods=['POST'])
+def askQuestionFunction(profileUserName):
+    userData = User.query.filter_by(userName=profileUserName).first() # Getting Data of User
+    askedQuestionId = uuid.uuid4().hex # Creating Question Id of Question
+    askedQuestionTitle = request.form['questionTitle'] # Getting Question Title
+    askedQuestionDescription = request.form['questionDescription'] # Getting Question Description
+    askedQuestionCategory = request.form['selectACategory'] # Getting Category Id of the question
+    
+    # Checking if question is having a title and a description
+    if len(askedQuestionTitle) < 0 or len(askedQuestionDescription)<0:
+        print('asjs')
+        flash('The Question must have a title and a description',category='error') # Flashing Error message if question is not having a title and a description
+        return redirect(f'/profile/{profileUserName}') # Returning to same URL if question is not having a title or description
+    else:
+        newQuestion = Question(questionId=askedQuestionId,questionTitle=askedQuestionTitle,questionDescription=askedQuestionDescription,userNameOfAsker=profileUserName,realNameOfAsker=userData.realNameOfUser,categoryId=askedQuestionCategory) # Creating Instance of Question Object of Database
+
+        try:
+            db.session.add(newQuestion)
+            db.session.commit()
+            flash('Question Added Succesfully',category='success')
+            return redirect(f'/question/{askedQuestionId}') # Redirecting to questions page
+
+        except:
+            flash('Some Error Occured while adding the question',category='error')
+            return redirect(f'/profile/{profileUserName}') # Redirecting to questions page
--- views/profile.py
+++ views/profile.py
@@ -1,14 +1,16 @@
-from flask import Blueprint,render_template # Importing Blueprint to handle routes related to profile pages
+from flask import Blueprint,request,render_template # Importing Blueprint to handle routes related to profile pages
 from flask_login import current_user # Importing Details of Current User using flask_login
 from .models import User, Question, Category
 
 profile = Blueprint('profile',__name__,template_folder='templates') # Creating a Blueprint so that it could be accesed from files from outside
 
 # Make '/profileId' route and rendering profile.html page with specific data according to Id
-@profile.route('/<string:profileId>')
+@profile.route('/<string:profileId>',methods=['GET','POST'])
 def profilePageFunction(profileId):
+    if request.method == 'POST':
+        pass
     userObject = User.query.filter_by(userName=profileId).first()
-    userQuestions = Question.query.filter_by(userNameOfAsker=profileId)
+    userQuestions = Question.query.filter_by(userNameOfAsker=profileId).all()
     hasUserAskedQuestions = Question.query.filter_by(userNameOfAsker=profileId).count() # Counting wether the user has asked some questons
     if hasUserAskedQuestions == 0:
         return render_template('profile.html',user=current_user,profileUser=userObject,noQuestions=True,categoryData=Category.query.all()) # Rendering HTML Page
