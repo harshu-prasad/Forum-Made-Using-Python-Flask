cbbefd3ae131921125407d1d4d7f898bc915a45c
Harshu Prasad Shukla <84216006+Harshu9892@users.noreply.github.com>
2022-01-16 22:41:20 +0530

    Commit

--- main.py
+++ main.py
@@ -5,8 +5,9 @@
 """ FILE DESCRIPTION : THIS IS THE MAIN FILE OF THE PROJECT THAT WILL RUN THE WEBSITE """
 
 from views import createApp # Importing createApp from __init__.py file in views directory
+import os
 
 app = createApp() # Creating app using function createApp from __init__.py file in views directory
 
 if __name__ == '__main__':
-    app.run() # Runs Server at port 5000
+    app.run(host='0.0.0.0',debug=False,port=int(os.environ.get("PORT", 5000))) # Runs Server at port 5000
