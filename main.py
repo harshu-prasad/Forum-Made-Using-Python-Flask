8e980ed045d45584505692a36ab5b7aa099c7871
Harshu Prasad Shukla <84216006+Harshu9892@users.noreply.github.com>
2022-01-16 22:49:57 +0530

    commit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 ''' DESCRIPTION : This is a forum made using Python Flask for backend. For the Frontend Bootstrap has been extensively used with HTML, CSS and Javascript '''
 ''' AUTHOR : Harshu Prasad Shukla '''
 
+
 """ FILE DESCRIPTION : THIS IS THE MAIN FILE OF THE PROJECT THAT WILL RUN THE WEBSITE """
 
 from views import createApp # Importing createApp from __init__.py file in views directory
